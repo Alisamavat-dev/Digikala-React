f3ffa208791416b9c993d1bb63cecb459e253670
Alisamavat-dev <alisamavat.dev@gmail.com>
2025-04-23 12:09:07 +0330

    v21

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import Router from "./components/Router/Router.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={Router} />
+      <SpeedInsights />
     </QueryClientProvider>
   </StrictMode>
 );
